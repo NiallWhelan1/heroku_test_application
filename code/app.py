e56e8e69bb89cf10e9f267018f2b8aa6e968f800
Niall Whelan <niall.whelan@gmail.com>
2020-06-25 12:10:51 +0900

    Moved db.init() out of __main__ if requirement

--- code/app.py
+++ code/app.py
@@ -34,9 +34,10 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(UserList, '/users')
 
-if __name__ == '__main__':
-	db.init_app(app)
 
+db.init_app(app)
+
+if __name__ == '__main__':	
 	app.run(host = '127.0.0.1',	
 			port = 5000,
 			debug = True
